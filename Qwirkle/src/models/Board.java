7fcaeb2021c579c26b75a53ead4c45b0f80a341b
Arend Pool <arend.w.pool@gmail.com>
2016-01-12 18:53:23 +0100

    Totale overhaul van de package lay-out. Misschien te veel of te weinig packages, maar daar komen we wel achter.

---
+++ Qwirkle/src/models/Board.java
@@ -0,0 +1,70 @@
+package models;
+/**
+ *De klasse die een speelbord representeerd maakt met een naam en tegels.
+ * @author Bob Breemhaar en Arend Pool.
+ *
+ */
+public class Board {
+	public static final int DIM = 0;
+	public static final int MID = 0;
+    
+    private	Tiles[] fields = new Tiles[DIM * DIM];
+	
+	/*
+	 * Constructor van het Board
+	 */
+	public Board(){	
+	
+	}
+	
+	/*
+	 * creeërt een kopie van het speelveld
+	 */
+	public Board deepCopy(){
+		return null;
+	}
+	
+	/*
+	 * berekend een index.
+	 */
+	public int index(){
+		return 0;
+	}
+	
+	/* 
+	 * returns de inhoud van field i.
+	 */
+	public Tiles getField(int i){
+		return null;
+	}
+	
+	/*
+	 * returns de inhoud verwezen door het paar (x,y) .
+	 */
+	public Tiles getField(int y, int x){
+		return null;
+	}
+	
+	/*
+	 * return true als een field i leeg is.
+	 */
+	public boolean isEmptyField(int i){
+		return false;
+	}
+	
+	/*
+	 * returns true als een verwezen field pair(x,y) leeg is
+	 */
+	public boolean isEmptyField(int y, int x){
+		return false;
+	}
+	
+	/* 
+	 * Moet de Move van de player verwerken.
+	 */
+	public void makeMove(){
+		
+	}
+	
+
+}
---
+++ Qwirkle/src/models/ComputerPlayer.java
@@ -0,0 +1,9 @@
+package models;
+
+public class ComputerPlayer extends Player {
+	private static final String name = "Computer";
+	public ComputerPlayer() {
+		super(name);
+	}
+
+}
