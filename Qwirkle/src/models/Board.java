c54fcc7d4ed84f2e79c3407f1964e6eb2d2392d1
Arend Pool <arend.w.pool@gmail.com>
2016-01-13 14:28:09 +0100

    Aantal aanpassingen aan Board.java en Game.java. Verder is de package "ui" hernoemd als "view" en "Tiles" is hernoemd als "Tile".

--- Qwirkle/src/models/Board.java
+++ Qwirkle/src/models/Board.java
@@ -1,11 +1,14 @@
 package models;
+
+import exceptions.InvalidMoveException;
+
 /**
  *De klasse die een speelbord representeerd maakt met een naam en tegels.
  * @author Bob Breemhaar en Arend Pool.
  *
  */
 public class Board {
-	private	Tiles[] [] coordinaten;
+	private	Tile[] [] coordinaten;
 	private static final int DIM = 179;
 	private boolean initialMove;
 
@@ -21,7 +24,7 @@ public class Board {
 	 * creeërt een kopie van het speelveld
 	 */
 	public Board deepCopy(){
-		coordinaten = new Tiles[DIM][DIM];
+		coordinaten = new Tile[DIM][DIM];
 		for (int x = 0; x < coordinaten.length; x++) {
 			for (int y = 0; y < coordinaten.length; y++) {
 				coordinaten[x][y] = null;
@@ -38,7 +41,7 @@ public class Board {
 	/*
 	 * returns de inhoud verwezen door het paar (x,y) .
 	 */
-	public Tiles getField(int x, int y){
+	public Tile getField(int x, int y){
 		return coordinaten[x][y];
 	}
 	
@@ -52,10 +55,11 @@ public class Board {
 	
 	
 	
-	public boolean isValidMove(int x, int y, Tiles tileA, Tiles tileB) throws InvalidMoveException{
+	public boolean isValidMove(int x, int y, Tile tileA, Tile tileB){
+		//Tile controlTile = ;
+		//TODO implementatie weizigen zodat deze compitabel is met arraylists
 		if(isEmptyField(x, y) == false){
 			return false;
-			throw new InvalidMoveException();
 		}else if(initialMove == true && x == 90 && y == 90){
 			initialMove = false;
 			return true;
@@ -73,22 +77,26 @@ public class Board {
 		}//TODO Meer invalid shit vinden
 	}
 	
-	public boolean compareSymbool(Tiles tileA, Tiles tileB){
+	public boolean compareSymbool(Tile tileA, Tile tileB){
 		return(tileA.getSymbol() == tileB.getSymbol());
 		
 	}
 	
-	public boolean compareColor(Tiles tileA, Tiles tileB){
+	public boolean compareColor(Tile tileA, Tile tileB){
 		return(tileA.getColor() == tileB.getColor());
 	}
 	
 	/* 
 	 * Moet de Move van de player verwerken.
 	 */
-	public void processMove(int x, int y, Tiles tile){
+	public void processMove(int x, int y, Tile tile) throws InvalidMoveException{
+		if(isValidMove(x, y, tile, new Tile("kleur", "symbool"))){
+			//TODO veranderen zodat hij checkt of de move valid is
 			coordinaten[x][y] = tile;
-		
-		
+		}else{
+			throw new InvalidMoveException();
+		}
+		//TODO aanpassen op een overzichtelijke manier: welke tile wordt gezet en welke is ter controle
 	}
 	
 	public void reset() {
