347d22bc1e387a8fcb3c426edda153411770e7f5
Arend Pool <arend.w.pool@gmail.com>
2016-01-14 21:01:14 +0100

    Specificatie van TUI af: een abstracte klasse TUI, en een subklasse ervan: BoardTUI

---
+++ Qwirkle/src/view/BoardTUI.java
@@ -0,0 +1,18 @@
+package view;
+
+import models.Game;
+
+public class BoardTUI extends TUI {
+
+	public BoardTUI(Game game) {
+		super(game);
+	}
+	
+	public void renderPrompt(int noOfPromtItems, String[] PromptItems){
+		
+	}
+	
+	public void updateBoard(){
+		
+	}
+}
